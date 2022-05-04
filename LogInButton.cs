1db8a58499a0e752b7dc0e63a090bc88cdfdeffc
sameerrahmani <56892116+Sameerrahmani@users.noreply.github.com>
2022-05-04 09:25:15 -0400

    Update LogInButton.cs

--- LogInButton.cs
+++ LogInButton.cs
@@ -4,26 +4,29 @@ using System;
 public class LogInButton : Button
 {
 // Variables
-private Button LogIn;
-private Panel MenuPanel;
-private Panel LogPanel;
-private Tween tween;
+	private Button LogIn;
+	private Panel MenuPanel;
+	private Panel LogPanel;
 
 	public override void _Ready()
 	{
 		// Getting the nodes of the Login button and other panels.
-		LogIn = GetNode<Button>("LogInButton");
+		LogIn = GetNode<Button>("/root/Control/Background/MenuPanel/LogInButton");
 		MenuPanel = GetNode<Panel>("/root/Control/Background/MenuPanel");
 		LogPanel = GetNode<Panel>("/root/Control/Background/LogPanel");
-		tween = GetNode<Tween>("/root/Control/Background/MenuPanel/Tween");
 	}
-
-	// On login button press...
-	private void OnLogInButtonPressed()
+	
+	private void _on_LogInButton_pressed()
 	{
-	GD.Print("Log In Button Pressed");
-	MenuPanel.Visible = false;
-	LogPanel.Visible = true;
+		// on button press, takes you to the log in panel.
+		MenuPanel.Visible = false;
+		LogPanel.Visible = true;
 	}
 }
+
+
+
+
+
+
 
